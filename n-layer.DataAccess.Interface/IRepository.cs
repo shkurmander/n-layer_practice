1fa78b64e10966884cab836c6ec14d4599503d3f
Vladimir Berdnikov <gazetter@mail.ru>
2021-07-24 03:58:17 +0700

    Переписал интерфейсы, репозиторий, методы на всех 3 уровнях переписал файловую базу на работу с json, все работает

---
+++ n-layer.DataAccess.Interface/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using n_layer.Entities;
+using System.Collections.Generic;
+
+
+
+namespace n_layer.DataAccess
+{
+    public interface  IRepository<T> where T : class
+    {
+       
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        IEnumerable<T> Find(string name);
+        T Find(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+        void Save(); 
+
+    }
+}
---
+++ n-layer.DataAccess/FileDb/FileContext.cs
@@ -0,0 +1,44 @@
+﻿using n_layer.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace n_layer.DataAccess
+{
+    public class FileContext
+    {
+        private string _path;
+
+        public string FilePath { get => _path; }
+        public List<Target> Targets { get; set; }
+
+        public FileContext(string path)
+        {
+            _path = path;
+            Targets = new List<Target>();
+            if (File.Exists(_path))
+            {
+               
+                //Десериализуем данные из файла
+                using (var sr = new StreamReader(_path))
+                {
+                    try
+                    {
+                        string json = sr.ReadToEnd();
+                        Targets = JsonConvert.DeserializeObject<List<Target>>(json);
+                           
+                    }
+                    catch (Exception)
+                    {
+                        Targets = new List<Target>();
+                        Console.WriteLine("В файле нет данных!");
+                    }
+                }               
+                
+            }
+        }
+         
+        
+    }
+}
