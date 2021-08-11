f168f23973b02e6d1b1d252b28ca21ba8fefab43
Vladimir Berdnikov <gazetter@mail.ru>
2021-08-11 16:06:25 +0700

    Запилил EF в DAL, написал SQL репозиторий. Добавил в конфиг секцию со строкой подключения, прокинул строку подключения через конструкторы до DBContext'a

---
+++ n-layer.DataAccess/EF/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using n_layer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace n_layer.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        private string _dbconnection;
+        public DbSet<Target> Targets { get; set; }
+
+
+        public AppDbContext(string dbconnection)
+        {
+            _dbconnection = dbconnection;
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_dbconnection);
+        }
+    }
+}
